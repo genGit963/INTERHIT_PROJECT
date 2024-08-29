a3af8c3ebb0b6981154f6c6089e1b8b97fc33e5a
govin-10 <govindatimsina@outlook.com>
2024-08-29 18:14:27 +0545

    Android-portrait layout

--- App.tsx
+++ App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import AppNavigator from './src/navigation';
-import {LanguageContextProvider} from './src/context/language';
 import {Provider} from 'react-redux';
 import store from './src/redux/store';
 import {PersistGate} from 'redux-persist/integration/react';
 import {persistStore} from 'redux-persist';
-// import {StyleSheet} from 'react-native';
 
 export function App() {
   const persistor = persistStore(store);
@@ -13,9 +11,7 @@ export function App() {
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <LanguageContextProvider>
-          <AppNavigator />
-        </LanguageContextProvider>
+        <AppNavigator />
       </PersistGate>
     </Provider>
   );
