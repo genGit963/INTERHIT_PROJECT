5d9d43e37a43b2c49c4049a83b7af543f300f8ab
govin-10 <govindatimsina@outlook.com>
2024-08-29 18:16:08 +0545

    merged

--- App.tsx
+++ App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import AppNavigator from './src/navigation';
-
 import {Provider} from 'react-redux';
 import store from './src/redux/store';
 import {PersistGate} from 'redux-persist/integration/react';
-
 import {persistStore} from 'redux-persist';
 
 export function App() {
